684849cc112b809e23211bf7a76adb2ba1e1c0b1
Amaan <amaanreigns01@gmail.com>
2024-07-13 00:33:02 +0530

    My personal notes page structure is completed

---
+++ func.js
@@ -0,0 +1,24 @@
+function changetoh1() {
+  document.getElementById("output").innerHTML = "<h1>This is new Text</h1>";
+  document.getElementById("headingDiv").innerHTML = "h1 Tag";
+}
+function changetoh2() {
+  document.getElementById("output").innerHTML = "<h2>This is new Text</h2>";
+  document.getElementById("headingDiv").innerHTML = "h2 Tag";
+}
+function changetoh3() {
+  document.getElementById("output").innerHTML = "<h3>This is new Text</h3>";
+  document.getElementById("headingDiv").innerHTML = "h3 Tag";
+}
+function changetoh4() {
+  document.getElementById("output").innerHTML = "<h4>This is new Text</h4>";
+  document.getElementById("headingDiv").innerHTML = "h4 Tag";
+}
+function changetoh5() {
+  document.getElementById("output").innerHTML = "<h5>This is new Text</h5>";
+  document.getElementById("headingDiv").innerHTML = "h5 Tag";
+}
+function changetoh6() {
+  document.getElementById("output").innerHTML = "<h6>This is new Text</h6>";
+  document.getElementById("headingDiv").innerHTML = "h6 Tag";
+}
