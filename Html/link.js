aa71a6f6f39431d532156302c43c0b42692ee452
Amaan <amaanreigns01@gmail.com>
2024-07-18 22:52:14 +0530

    Meta tag page added

--- Html/link.js
+++ Html/link.js
@@ -3,3 +3,13 @@ function displaylinktag() {
   document.getElementById("linkoutput").innerHTML = `
           <a href="https://www.w3schools.com/" target="_blank">Visit W3Schools!</a>`;
 }
+function displaybookmarktag() {
+  document.getElementById("bookmarkoutput").style.display = "block";
+  document.getElementById("bookmarkoutput").innerHTML = `
+            <a href="#imagelinkbookmark">Image as Link</a>`;
+}
+function displayparabookmarktag() {
+  document.getElementById("parabookmarkoutput").style.display = "block";
+  document.getElementById("parabookmarkoutput").innerHTML = `
+              <a href="html_tags.html#paragraphbookmark">Paragraph Tag</a>`;
+}
