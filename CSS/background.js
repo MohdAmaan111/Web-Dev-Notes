610b84ee582ac494b8a2d398758bb2ffc0f1a36e
Amaan <amaanreigns01@gmail.com>
2024-08-04 13:02:38 +0530

    css background notes added

---
+++ CSS/background.js
@@ -0,0 +1,228 @@
+function bg_p_1() {
+  document.getElementById("headingDiv").innerHTML =
+    "background-position: left top;";
+  document.getElementById("output").innerHTML = `<head>
+    <style>
+        .bg-p-1 {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-size: 10%;
+            background-repeat: no-repeat;
+            background-position: left top;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-p-1"></div>
+</body>`;
+}
+function bg_p_2() {
+  document.getElementById("headingDiv").innerHTML =
+    "background-position: left center;";
+  document.getElementById("output").innerHTML = `<head>
+    <style>
+        .bg-p-1 {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-size: 10%;
+            background-repeat: no-repeat;
+            background-position: left center;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-p-1"></div>
+</body>`;
+}
+function bg_p_3() {
+  document.getElementById("headingDiv").innerHTML =
+    "background-position: left bottom;";
+  document.getElementById("output").innerHTML = `<head>
+    <style>
+        .bg-p-1 {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-size: 10%;
+            background-repeat: no-repeat;
+            background-position: left bottom;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-p-1"></div>
+</body>`;
+}
+function bg_p_4() {
+  document.getElementById("headingDiv").innerHTML =
+    "background-position: right top;";
+  document.getElementById("output").innerHTML = `<head>
+    <style>
+        .bg-p-1 {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-size: 10%;
+            background-repeat: no-repeat;
+            background-position: right top;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-p-1"></div>
+</body>`;
+}
+function bg_p_5() {
+  document.getElementById("headingDiv").innerHTML =
+    "background-position: right center;";
+  document.getElementById("output").innerHTML = `<head>
+    <style>
+        .bg-p-1 {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-size: 10%;
+            background-repeat: no-repeat;
+            background-position: right center;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-p-1"></div>
+</body>`;
+}
+function bg_p_6() {
+  document.getElementById("headingDiv").innerHTML =
+    "background-position: right bottom;";
+  document.getElementById("output").innerHTML = `<head>
+    <style>
+        .bg-p-1 {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-size: 10%;
+            background-repeat: no-repeat;
+            background-position: right bottom;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-p-1"></div>
+</body>`;
+}
+function bg_p_7() {
+  document.getElementById("headingDiv").innerHTML =
+    "background-position: center top;";
+  document.getElementById("output").innerHTML = `<head>
+    <style>
+        .bg-p-1 {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-size: 10%;
+            background-repeat: no-repeat;
+            background-position: center top;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-p-1"></div>
+</body>`;
+}
+function bg_p_8() {
+  document.getElementById("headingDiv").innerHTML =
+    "background-position: center center;";
+  document.getElementById("output").innerHTML = `<head>
+    <style>
+        .bg-p-1 {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-size: 10%;
+            background-repeat: no-repeat;
+            background-position: center center;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-p-1"></div>
+</body>`;
+}
+function bg_p_9() {
+  document.getElementById("headingDiv").innerHTML =
+    "background-position: center bottom;";
+  document.getElementById("output").innerHTML = `<head>
+    <style>
+        .bg-p-1 {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-size: 10%;
+            background-repeat: no-repeat;
+            background-position: center bottom;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-p-1"></div>
+</body>`;
+}
+function bg_p_10() {
+  document.getElementById(
+    "headingDiv"
+  ).innerHTML = `<p>The first value is the horizontal position and the second value is the vertical.</p>
+    <p>The top left corner is 0% 0%. The right bottom corner is 100% 100%.</p>
+    <p>If you only specify one value, the other value will be 50%. </p>
+    background-position: 10% 50%;`;
+  document.getElementById("output").innerHTML = `<head>
+    <style>
+        .bg-p-1 {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-size: 10%;
+            background-repeat: no-repeat;
+            background-position: 10% 50%;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-p-1"></div>
+</body>`;
+}
+function bg_p_11() {
+  document.getElementById(
+    "headingDiv"
+  ).innerHTML = `<p>The first value is the horizontal position and the second value is the vertical.</p>
+    <p>The top left corner is 0% 0%. The right bottom corner is 100% 100%.</p>
+    <p>If you only specify one value, the other value will be 50%. </p>
+    background-position: 150px 80px;`;
+  document.getElementById("output").innerHTML = `<head>
+    <style>
+        .bg-p-1 {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-size: 10%;
+            background-repeat: no-repeat;
+            background-position: 150px 80px;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-p-1"></div>
+</body>`;
+}
