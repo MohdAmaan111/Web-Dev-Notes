93d29bbbe74c8889dd59d1b7480afc3a3bd819e8
Amaan <amaanreigns01@gmail.com>
2024-08-07 08:00:52 +0530

    margin and padding notes added in css

--- CSS/background.js
+++ CSS/background.js
@@ -226,3 +226,234 @@ function bg_p_11() {
     <div class="bg-p-1"></div>
 </body>`;
 }
+function bg_s_1() {
+  document.getElementById(
+    "bg_s_headingDiv"
+  ).innerHTML = `<p>Default value. The background image is displayed in its original size.</p>`;
+  document.getElementById("bg_s_output").innerHTML = `<head>
+    <style>
+        .bg-size {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-repeat: no-repeat;
+            background-size: auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-size"></div>
+</body>`;
+}
+function bg_s_2() {
+  document.getElementById(
+    "bg_s_headingDiv"
+  ).innerHTML = `<p>Sets the width and height of the background image.</p>
+  <p>The first value sets the width, the second value sets the height.</p>
+  <p>If only one value is given, the second is set to "auto".</p>
+  <p>background-size: 10rem;</p>`;
+  document.getElementById("bg_s_output").innerHTML = `<head>
+    <style>
+        .bg-size {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-repeat: no-repeat;
+            background-size: 10rem;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-size"></div>
+</body>`;
+}
+function bg_s_3() {
+  document.getElementById(
+    "bg_s_headingDiv"
+  ).innerHTML = `<p>Sets the width and height of the background image in percent of the parent element.</p>
+  <p>The first value sets the width, the second value sets the height.</p>
+  <p>If only one value is given, the second is set to "auto".</p>
+  <p>background-size: 10%;</p>`;
+  document.getElementById("bg_s_output").innerHTML = `<head>
+    <style>
+        .bg-size {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-repeat: no-repeat;
+            background-size: 10%;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-size"></div>
+</body>`;
+}
+function bg_s_4() {
+  document.getElementById(
+    "bg_s_headingDiv"
+  ).innerHTML = `<p>SResize the background image to cover the entire container, 
+  even if it has to stretch the image or cut a little bit off one of the edges</p>
+  <p>background-size: cover;</p>`;
+  document.getElementById("bg_s_output").innerHTML = `<head>
+    <style>
+        .bg-size {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-size"></div>
+</body>`;
+}
+function bg_s_5() {
+  document.getElementById(
+    "bg_s_headingDiv"
+  ).innerHTML = `<p>Resize the background image to make sure the image is fully visible.</p>
+  <p>background-size: contain;</p>`;
+  document.getElementById("bg_s_output").innerHTML = `<head>
+    <style>
+        .bg-size {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-repeat: no-repeat;
+            background-size: contain;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-size"></div>
+</body>`;
+}
+function bg_a_f() {
+  document.getElementById(
+    "bg_a_headingDiv"
+  ).innerHTML = `background-attachment: fixed;`;
+  document.getElementById("bg_a_output").innerHTML = `<head>
+    <style>
+        .bg-attachment {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-size: contain;
+            background-position: 70% 0%;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-attachment"></div>
+</body>`;
+}
+function bg_a_s() {
+  document.getElementById(
+    "bg_a_headingDiv"
+  ).innerHTML = `background-attachment: scroll;`;
+  document.getElementById("bg_a_output").innerHTML = `<head>
+    <style>
+        .bg-attachment {
+            width: 100%;
+            height: 80%;
+            border: 2px solid white;
+            background-image: url("../Images/Goku.jpg");
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-attachment: scroll;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-attachment"></div>
+</body>`;
+}
+function bg_o_1() {
+  document.getElementById(
+    "bg_o_headingDiv"
+  ).innerHTML = `<p>Default value. The background image starts from the upper left corner 
+  of the padding edge. </p>`;
+  document.getElementById("bg_o_output").innerHTML = `<head>
+    <style>
+        .bg-origin {
+          width: 100%;
+          height: 80%;
+          border: 10px dashed white;
+          padding: 2rem;
+          color: red;
+          background-image: url("../Images/Goku.jpg");
+          background-size: 10rem;
+          background-repeat: no-repeat;
+          background-origin: padding-box;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-origin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+    repellendus rem atque dignissimos minus quaerat quibusdam nulla
+    voluptatibus molestias dolore optio nisi dolorum unde alias illo
+    sequi, voluptatem labore voluptate.</div>
+</body>`;
+}
+function bg_o_2() {
+  document.getElementById(
+    "bg_o_headingDiv"
+  ).innerHTML = `<p>The background image starts from the upper left corner of the border. </p>`;
+  document.getElementById("bg_o_output").innerHTML = `<head>
+    <style>
+        .bg-origin {
+          width: 100%;
+          height: 80%;
+          border: 10px dashed white;
+          padding: 2rem;
+          color: red;
+          background-image: url("../Images/Goku.jpg");
+          background-size: 10rem;
+          background-repeat: no-repeat;
+          background-origin: border-box;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-origin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+    repellendus rem atque dignissimos minus quaerat quibusdam nulla
+    voluptatibus molestias dolore optio nisi dolorum unde alias illo
+    sequi, voluptatem labore voluptate.</div>
+</body>`;
+}
+function bg_o_3() {
+  document.getElementById(
+    "bg_o_headingDiv"
+  ).innerHTML = `<p>The background image starts from the upper left corner of the content. </p>`;
+  document.getElementById("bg_o_output").innerHTML = `<head>
+    <style>
+        .bg-origin {
+          width: 100%;
+          height: 80%;
+          border: 10px dashed white;
+          padding: 2rem;
+          color: red;
+          background-image: url("../Images/Goku.jpg");
+          background-size: 10rem;
+          background-repeat: no-repeat;
+          background-origin: content-box;
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-origin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+    repellendus rem atque dignissimos minus quaerat quibusdam nulla
+    voluptatibus molestias dolore optio nisi dolorum unde alias illo
+    sequi, voluptatem labore voluptate.</div>
+</body>`;
+}
---
+++ CSS/borders.js
@@ -0,0 +1,120 @@
+function b_w_1() {
+  document.getElementById("borderWidthHeading").innerHTML = "2px";
+  document.getElementById("contentBorder").style.borderWidth = "2px";
+}
+function b_w_2() {
+  document.getElementById("borderWidthHeading").innerHTML = "4px";
+  document.getElementById("contentBorder").style.borderWidth = "4px";
+}
+function b_w_3() {
+  document.getElementById("borderWidthHeading").innerHTML = "6px";
+  document.getElementById("contentBorder").style.borderWidth = "6px";
+}
+function b_w_4() {
+  document.getElementById("borderWidthHeading").innerHTML = "8px";
+  document.getElementById("contentBorder").style.borderWidth = "8px";
+}
+function b_w_5() {
+  document.getElementById("borderWidthHeading").innerHTML = "10px";
+  document.getElementById("contentBorder").style.borderWidth = "10px";
+}
+function b_w_6() {
+  document.getElementById("borderWidthHeading").innerHTML = "12px";
+  document.getElementById("contentBorder").style.borderWidth = "12px";
+}
+function b_w_7() {
+  document.getElementById("borderWidthHeading").innerHTML = "14px";
+  document.getElementById("contentBorder").style.borderWidth = "14px";
+}
+function b_w_8() {
+  document.getElementById("borderWidthHeading").innerHTML = "16px";
+  document.getElementById("contentBorder").style.borderWidth = "16px";
+}
+function b_w_9() {
+  document.getElementById("borderWidthHeading").innerHTML = "18px";
+  document.getElementById("contentBorder").style.borderWidth = "18px";
+}
+function b_w_10() {
+  document.getElementById("borderWidthHeading").innerHTML = "20px";
+  document.getElementById("contentBorder").style.borderWidth = "20px";
+}
+function b_s_1() {
+  document.getElementById("borderStyleHeading").innerHTML = "dotted";
+  document.getElementById("contentBorder").style.borderStyle = "dotted";
+}
+function b_s_2() {
+  document.getElementById("borderStyleHeading").innerHTML = "dashed";
+  document.getElementById("contentBorder").style.borderStyle = "dashed";
+}
+function b_s_3() {
+  document.getElementById("borderStyleHeading").innerHTML = "solid";
+  document.getElementById("contentBorder").style.borderStyle = "solid";
+}
+function b_s_4() {
+  document.getElementById("borderStyleHeading").innerHTML = "double";
+  document.getElementById("contentBorder").style.borderStyle = "double";
+}
+function b_s_5() {
+  document.getElementById("borderStyleHeading").innerHTML = "groove";
+  document.getElementById("contentBorder").style.borderStyle = "groove";
+}
+function b_s_6() {
+  document.getElementById("borderStyleHeading").innerHTML = "ridge";
+  document.getElementById("contentBorder").style.borderStyle = "ridge";
+}
+function b_s_7() {
+  document.getElementById("borderStyleHeading").innerHTML = "inset";
+  document.getElementById("contentBorder").style.borderStyle = "inset";
+}
+function b_s_8() {
+  document.getElementById("borderStyleHeading").innerHTML = "outset";
+  document.getElementById("contentBorder").style.borderStyle = "outset";
+}
+function b_s_9() {
+  document.getElementById("borderStyleHeading").innerHTML = "hidden";
+  document.getElementById("contentBorder").style.borderStyle = "hidden";
+}
+function b_s_10() {
+  document.getElementById("borderStyleHeading").innerHTML = "none";
+  document.getElementById("contentBorder").style.borderStyle = "none";
+}
+function b_c_1() {
+  document.getElementById("borderColorHeading").innerHTML = "red";
+  document.getElementById("contentBorder").style.borderColor = "red";
+}
+function b_c_2() {
+  document.getElementById("borderColorHeading").innerHTML = "blue";
+  document.getElementById("contentBorder").style.borderColor = "blue";
+}
+function b_c_3() {
+  document.getElementById("borderColorHeading").innerHTML = "green";
+  document.getElementById("contentBorder").style.borderColor = "green";
+}
+function b_c_4() {
+  document.getElementById("borderColorHeading").innerHTML = "yellow";
+  document.getElementById("contentBorder").style.borderColor = "yellow";
+}
+function b_c_5() {
+  document.getElementById("borderColorHeading").innerHTML = "black";
+  document.getElementById("contentBorder").style.borderColor = "black";
+}
+function b_c_6() {
+  document.getElementById("borderColorHeading").innerHTML = "gray";
+  document.getElementById("contentBorder").style.borderColor = "gray";
+}
+function b_c_7() {
+  document.getElementById("borderColorHeading").innerHTML = "pink";
+  document.getElementById("contentBorder").style.borderColor = "pink";
+}
+function b_c_8() {
+  document.getElementById("borderColorHeading").innerHTML = "beige";
+  document.getElementById("contentBorder").style.borderColor = "beige";
+}
+function b_c_9() {
+  document.getElementById("borderColorHeading").innerHTML = "skyblue";
+  document.getElementById("contentBorder").style.borderColor = "skyblue";
+}
+function b_c_10() {
+  document.getElementById("borderColorHeading").innerHTML = "white";
+  document.getElementById("contentBorder").style.borderColor = "white";
+}
